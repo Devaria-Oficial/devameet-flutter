21e3af478e27982ad977f9509bdc025826a0c520
Alan Franzin <alan.franzin2011@gmail.com>
2023-08-23 22:56:37 -0300

    Login - estelizacao - parte 02

---
+++ lib/constants/color.dart
@@ -0,0 +1,8 @@
+
+
+import 'package:flutter/material.dart';
+
+abstract class DColors {
+  static final primary3 = Color(0xFF25CBD3);
+  static final grey2 = Color(0xFF7C7786);
+}
--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,4 +1,6 @@
 
+import 'package:devameet_flutter/components/shared/input_field.dart';
+import 'package:devameet_flutter/constants/color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -23,18 +25,21 @@ class LoginPage extends StatelessWidget {
 
           Container(
             margin: EdgeInsets.only(top: height * 0.12),
-            child: Form(
-              child: Column(
-                children: [
-                  TextFormField(),
-                  TextFormField(),
-                  Container(
-                    margin: EdgeInsets.only(top: height * 0.0625),
-                    child: ElevatedButton(onPressed: () => {}, child: Text("Login")),
-                  )
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Form(
+                child: Column(
+                  children: [
+                    InputField(hint: "Email", iconPath: "assets/icons/envelope.svg"),
+                    InputField(hint: "Senha", iconPath: "assets/icons/key.svg"),
+                    Container(
+                      margin: EdgeInsets.only(top: height * 0.0625),
+                      child: ElevatedButton(onPressed: () => {}, child: Text("Login")),
+                    )
 
-                ],
-              )
+                  ],
+                )
+              ),
             )
           ),
 
