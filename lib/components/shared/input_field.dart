85be158a20b75c6fe61d94912b2640a900dd2a12
Alan Franzin <alan.franzin2011@gmail.com>
2023-08-24 22:19:27 -0300

    login cubit dependencies

--- lib/components/shared/input_field.dart
+++ lib/components/shared/input_field.dart
@@ -6,26 +6,31 @@ class InputField extends StatelessWidget {
 
   final String hint;
   final String iconPath;
+  final
 
   const InputField({super.key, required this.hint, required this.iconPath});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          border: Border(bottom: BorderSide(color: DColors.primary3, width: 1))
-      ),
-      child: TextFormField(
-        style: TextStyle(color: DColors.primary3),
-        decoration: InputDecoration(
-            contentPadding: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
-            icon: SvgPicture.asset(iconPath),
-            hintText: hint,
-            hintStyle: TextStyle(color: DColors.grey2),
-            border: InputBorder.none
+    return StreamBuilder(builder: (context, snapshot ) {
+      return Container(
+        decoration: BoxDecoration(
+            border: Border(bottom: BorderSide(color: DColors.primary3, width: 1))
         ),
-      ),
-    );
+        child: TextFormField(
+          style: TextStyle(color: DColors.primary3),
+          decoration: InputDecoration(
+              contentPadding: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+              icon: SvgPicture.asset(iconPath),
+              hintText: hint,
+              hintStyle: TextStyle(color: DColors.grey2),
+              border: InputBorder.none
+          ),
+        ),
+      );
+
+    });
+
   }
 
 }
