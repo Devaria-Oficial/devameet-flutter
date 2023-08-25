3224cecf6c36f10a5b9f2adb42c8b3c6d2d01de0
Alan Franzin <alan.franzin2011@gmail.com>
2023-08-25 15:54:16 -0300

    Servico de http - parte 2

--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -1,5 +1,6 @@
 
 
+import 'dart:io';
 import 'package:dartz/dartz.dart';
 import 'package:devameet_flutter/errors/failures.dart';
 import 'package:dio/dio.dart';
@@ -8,6 +9,11 @@ import 'package:dio/dio.dart';
 
 abstract class HttpService {
   Future<Either<Failure, Response>> request({required String path, required String method, Map<String, dynamic>? data });
+}
+
+abstract class HttpMessages {
+  static final INTERNAL_ERROR = "Erro Interno";
+  static final CONNECTION_ERROR = "Não foi possivel estabelecer uma conexão com o servidor. Tente novamente mais tarde!";
 }
 
 class HttpClientImpl implements HttpService {
@@ -24,9 +30,23 @@ class HttpClientImpl implements HttpService {
       final response = await client.request(path, data: data, options: Options(method: method));
       return Right(response);
     } on DioException catch(e) {
-      return Left(ApiFailure(statusCode: 500, response: e.response));
+
+      if (e.isNoConnectionError) {
+        return Left(ApiFailure( statusCode: 500, message: HttpMessages.CONNECTION_ERROR));
+      }
+
+      if (e.response == null) {
+        return Left(ApiFailure(statusCode: 500, message: HttpMessages.INTERNAL_ERROR));
+      }
+
+      return Left(ApiFailure(statusCode: e.response?.statusCode as int, response: e.response));
+
     }
   }
+}
+
+extension DioExceptionX on DioException {
+  bool get isNoConnectionError => type == DioExceptionType.unknown && error is SocketException;
 }
 
 
