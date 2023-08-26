6672a2a66b72d52fce2392ac29138eea359c7d2c
Alan Franzin <alan.franzin2011@gmail.com>
2023-08-26 13:37:41 -0300

    Cadastro - Modal de Avatar - Parte 2

--- lib/components/shared/select_avatar.dart
+++ lib/components/shared/select_avatar.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:devameet_flutter/constants/color.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -9,18 +7,42 @@ class SelectAvatar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
 
     return AlertDialog(
       title: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text("Avatar", style: TextStyle(color: DColors.grey2, fontSize: 12, fontWeight: FontWeight.w600),),
-          Text("Selecione seu avatar", style: TextStyle(color: DColors.primary3, fontSize: 16, fontWeight: FontWeight.w700))
+          Text(
+            "Avatar",
+            style: TextStyle(
+                color: DColors.grey2,
+                fontSize: 12,
+                fontWeight: FontWeight.w600),
+          ),
+          Text("Selecione seu avatar",
+              style: TextStyle(
+                  color: DColors.primary3,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700))
         ],
       ),
-      content: Container(),
+      content: Container(
+        width: width,
+        child: GridView.count(
+            crossAxisSpacing: 7.79,
+            mainAxisSpacing: 7.79,
+            crossAxisCount: 3,
+            children: List.generate(
+                9,
+                (index) => Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all((Radius.circular(4))),
+                        color: DColors.white2,
+                        border: index == 4 ? Border.all(color: DColors.primary3, width: 2) : null
+                    )))),
+      ),
       actions: [],
     );
   }
-
 }
--- lib/constants/color.dart
+++ lib/constants/color.dart
@@ -8,6 +8,7 @@ abstract class DColors {
   static final grey2 = Color(0xFF7C7786);
   static final grey3 = Color(0xFF4D4C4D);
   static final white = Color(0xFFFFFFFF);
+  static final white2 = Color(0xFFF0F5FF);
 
   static final redError = Color(0xFFFF0033);
 }
