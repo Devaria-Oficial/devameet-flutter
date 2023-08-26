b6a1f8bd5e66d453dcb2c8288852b1742b5a7255
Alan Franzin <alan.franzin2011@gmail.com>
2023-08-26 14:02:00 -0300

    Cadastro - Modal de Avatar - Parte 3

--- lib/components/shared/select_avatar.dart
+++ lib/components/shared/select_avatar.dart
@@ -8,6 +8,7 @@ class SelectAvatar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
 
     return AlertDialog(
       title: Column(
@@ -24,7 +25,9 @@ class SelectAvatar extends StatelessWidget {
               style: TextStyle(
                   color: DColors.primary3,
                   fontSize: 16,
-                  fontWeight: FontWeight.w700))
+                  fontWeight: FontWeight.w700)),
+          SizedBox(height: height * 0.0025),
+          Divider()
         ],
       ),
       content: Container(
@@ -35,12 +38,18 @@ class SelectAvatar extends StatelessWidget {
             crossAxisCount: 3,
             children: List.generate(
                 9,
-                (index) => Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all((Radius.circular(4))),
-                        color: DColors.white2,
-                        border: index == 4 ? Border.all(color: DColors.primary3, width: 2) : null
-                    )))),
+                (index) => InkWell(
+                  onTap: () {},
+                  child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all((Radius.circular(4))),
+                          color: DColors.white2,
+                          border: index == 4 ? Border.all(color: DColors.primary3, width: 2) : null
+                      ),
+                      child: Image.asset("assets/devameet/Avatar/avatar_0${index+1}_front.png", fit: BoxFit.contain,),
+
+                  ),
+                ))),
       ),
       actions: [],
     );
