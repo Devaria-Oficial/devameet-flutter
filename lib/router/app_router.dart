3e7a2d58bd04a528fb667babb7dab84db6a3fa4f
Alan Franzin <alan.franzin2011@gmail.com>
2023-08-27 19:03:21 -0300

    Iniciando o Perfil

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,34 +1,14 @@
 import 'dart:async';
 
+import 'package:devameet_flutter/cubits/app/app_cubit.dart';
 import 'package:devameet_flutter/views/login.dart';
+import 'package:devameet_flutter/views/profile.dart';
 import 'package:devameet_flutter/views/register.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'package:go_router/go_router.dart';
 
-import '../cubits/app/app_cubit.dart';
-
-class PrivatePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: BlocBuilder<AppCubit, AppState>(builder: (context, state) {
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Exemplo de pagina privada"),
-              ElevatedButton(
-                  onPressed: context.read<AppCubit>().performLogout,
-                  child: Text("Deslogar"))
-            ],
-          ),
-        );
-      }),
-    );
-  }
-}
 
 class AppRouter {
   final AppCubit appCubit;
@@ -37,7 +17,7 @@ class AppRouter {
 
   late final GoRouter router = GoRouter(
       routes: [
-        GoRoute(path: "/", builder: (context, state) => PrivatePage()),
+        GoRoute(path: "/", builder: (context, state) => ProfilePage()),
         GoRoute(path: "/sign_up", builder: (context, state) => RegisterPage()),
         GoRoute(path: "/sign_in", builder: (context, state) => LoginPage()),
       ],
---
+++ lib/views/profile.dart
@@ -0,0 +1,42 @@
+
+
+import 'package:devameet_flutter/cubits/app/app_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ProfileView()
+      )
+    );
+  }
+
+}
+
+class ProfileView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(),
+        Container(),
+        Container(),
+        Spacer(),
+        Container(
+          child: ElevatedButton(
+            onPressed: context.read<AppCubit>().performLogout,
+            child: Text("Sair"),
+          ),
+        ),
+      ],
+    );
+  }
+  
+}
