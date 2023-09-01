ec69df0a5ab3159612dbfa47c87fe985de8aab7f
Alan Franzin <alan.franzin2011@gmail.com>
2023-09-01 16:48:29 -0300

    Lista de reunioes - Parte 5

--- lib/components/room/meets_section.dart
+++ lib/components/room/meets_section.dart
@@ -1,3 +1,4 @@
+import 'package:devameet_flutter/components/shared/button.dart';
 import 'package:devameet_flutter/constants/color.dart';
 import 'package:devameet_flutter/cubits/meet/meet_cubit.dart';
 import 'package:devameet_flutter/models/meet_model.dart';
@@ -12,21 +13,17 @@ class MeetsSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return RefreshIndicator(child: MeetList(), onRefresh: ()async{});
+    return RefreshIndicator(child: MeetList(), onRefresh: () async {});
   }
-
 }
-
 
 class MeetList extends StatelessWidget {
   const MeetList({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     return BlocBuilder<MeetCubit, MeetState>(builder: (context, state) {
-
-      if(state.meets.isEmpty) {
+      if (state.meets.isEmpty) {
         return const EmptyMeets();
       }
 
@@ -36,11 +33,8 @@ class MeetList extends StatelessWidget {
         itemBuilder: (_, index) => MeetItem(meet: state.meets[index]),
       );
     });
-
   }
-
 }
-
 
 class EmptyMeets extends StatelessWidget {
   const EmptyMeets({super.key});
@@ -54,20 +48,20 @@ class EmptyMeets extends StatelessWidget {
       child: Column(
         children: [
           SvgPicture.asset("assets/images/empty.svg"),
-          Text("Você ainda não possui reuniões criadas :(", style: TextStyle(
-            color: DColors.grey2,
-            fontSize: 14,
-            fontWeight: FontWeight.w600,
-            height: 2.85,
-            letterSpacing: 0.14
-          ),)
+          Text(
+            "Você ainda não possui reuniões criadas :(",
+            style: TextStyle(
+                color: DColors.grey2,
+                fontSize: 14,
+                fontWeight: FontWeight.w600,
+                height: 2.85,
+                letterSpacing: 0.14),
+          )
         ],
       ),
     );
   }
-
 }
-
 
 class MeetItem extends StatelessWidget {
   final MeetModel meet;
@@ -81,58 +75,147 @@ class MeetItem extends StatelessWidget {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: height * 0.025),
       decoration: BoxDecoration(
-        border: Border(bottom: BorderSide(color: DColors.grey1, width: 0.5))
-      ),
+          border: Border(bottom: BorderSide(color: DColors.grey1, width: 0.5))),
       child: ListTile(
-        contentPadding: const EdgeInsets.all(0),
-        minLeadingWidth: 0,
-        leading: Container(
-          width: width * 0.016666666666667,
-          height: height * 0.05,
-          decoration: BoxDecoration(
-            color: meet.color.toColor(),
-            borderRadius: BorderRadius.circular(12)
+          contentPadding: const EdgeInsets.all(0),
+          minLeadingWidth: 0,
+          leading: Container(
+            width: width * 0.016666666666667,
+            height: height * 0.05,
+            decoration: BoxDecoration(
+                color: meet.color.toColor(),
+                borderRadius: BorderRadius.circular(12)),
           ),
-        ),
-        title: Text(meet.name, style: TextStyle(
-          color: DColors.grey3,
-          fontSize: 12,
-          fontWeight: FontWeight.w700
-        ),),
-        trailing: Wrap(
-          spacing: width * 0.022222222222222,
-          children: [
-            InkWell(
-              splashColor: DColors.grey1,
-              onTap: () {},
-              child: SvgPicture.asset("assets/icons/door.svg", width: width * 0.044444444444444, color: DColors.grey2,),
-            ),
-            InkWell(
-              splashColor: DColors.grey1,
-              onTap: () {
-                Clipboard.setData(ClipboardData(text: meet.link)).then((_) {
-                  ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Link copiado!"),));
-                });
-              },
-              child: SvgPicture.asset("assets/icons/copy.svg", width: width * 0.044444444444444, color: DColors.grey2,),
-            ),
-            InkWell(
-              splashColor: DColors.grey1,
-              onTap: () {},
-              child: SvgPicture.asset("assets/icons/trash.svg", width: width * 0.044444444444444, color: DColors.grey2,),
-            ),
-          ],
-        )
-      ),
+          title: Text(
+            meet.name,
+            style: TextStyle(
+                color: DColors.grey3,
+                fontSize: 12,
+                fontWeight: FontWeight.w700),
+          ),
+          trailing: Wrap(
+            spacing: width * 0.022222222222222,
+            children: [
+              InkWell(
+                splashColor: DColors.grey1,
+                onTap: () {},
+                child: SvgPicture.asset(
+                  "assets/icons/door.svg",
+                  width: width * 0.044444444444444,
+                  color: DColors.grey2,
+                ),
+              ),
+              InkWell(
+                splashColor: DColors.grey1,
+                onTap: () {
+                  Clipboard.setData(ClipboardData(text: meet.link)).then((_) {
+                    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content: Text("Link copiado!"),
+                    ));
+                  });
+                },
+                child: SvgPicture.asset(
+                  "assets/icons/copy.svg",
+                  width: width * 0.044444444444444,
+                  color: DColors.grey2,
+                ),
+              ),
+              InkWell(
+                splashColor: DColors.grey1,
+                onTap: () {
+                  showDialog(
+                      context: context, builder: (_) => DeleteMeetModal());
+                },
+                child: SvgPicture.asset(
+                  "assets/icons/trash.svg",
+                  width: width * 0.044444444444444,
+                  color: DColors.grey2,
+                ),
+              ),
+            ],
+          )),
     );
   }
-
 }
 
 extension ColorExtension on String {
   Color toColor() {
     String hexColor = this.replaceAll("#", "0xFF");
     return Color(int.parse(hexColor));
+  }
+}
+
+class DeleteMeetModal extends StatelessWidget {
+  const DeleteMeetModal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+
+    return AlertDialog(
+      title: Center(
+          child: Text(
+        "Deletar reunião",
+        style: TextStyle(
+            color: DColors.primary3,
+            fontSize: 18.909,
+            fontWeight: FontWeight.w700,
+            height: 1.2),
+      )),
+      insetPadding: EdgeInsets.symmetric(
+          horizontal: width * 0.044444444444444, vertical: 216),
+      titlePadding: EdgeInsets.only(top: height * 0.0375, bottom: height * 0.025),
+      contentPadding: EdgeInsets.only(bottom: height * 0.0375),
+      actionsPadding: EdgeInsets.only(bottom: height * 0.0375),
+      content: Container(
+        width: width,
+        height: height * 0.075,
+        child: Column(
+          children: [
+            Text(
+              "Deseja deletar a reunião?",
+              style: TextStyle(
+                  color: DColors.grey2,
+                  fontSize: 16.545,
+                  fontWeight: FontWeight.w400,
+                  height: 1.45),
+            ),
+            Text("Essa ação não poderá ser desfeita.",
+                style: TextStyle(
+                    color: DColors.grey2,
+                    fontSize: 16.545,
+                    fontWeight: FontWeight.w400,
+                    height: 1.45))
+          ],
+        ),
+      ),
+      actions: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: width * 0.25,
+              child: Button(
+                text: "Cancelar",
+                onPressed: () => Navigator.pop(context, false),
+                isInverted: true,
+              ),
+            ),
+            SizedBox(
+              width: width * 0.01,
+            ),
+            SizedBox(
+              width: width * 0.41666666666667,
+              child: Button(
+                text: "Confirmar",
+                onPressed: () => Navigator.pop(context, false),
+              ),
+            )
+          ],
+        )
+      ],
+    );
   }
 }
